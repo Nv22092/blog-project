ddbba178c9fe0750502d08267cc1b357f48824fd
Nv22092 <154881721+Nv22092@users.noreply.github.com>
2024-04-26 13:49:38 +0000

    Custom error handler

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import {
+  errorResponserHandler,
+  invalidPathHandler,
+} from "./middleware/errorHandler";
 
 // Routes
 import userRoutes from "./routes/userRoutes";
@@ -15,6 +19,9 @@ app.get("/", (req, res) => {
   res.send("Server is running...");
 });
 app.use("/api/users", userRoutes);
+
+app.use(invalidPathHandler);
+app.use(errorResponserHandler);
 
 const PORT = process.env.PORT || 5000;
 
